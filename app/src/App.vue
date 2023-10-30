1256c1d5ee74dba29137e10a5b404a4e8d53b17f
nemanjamalesija <malesija13@gmail.com>
2023-10-30 13:47:08 +0100

    .

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,75 +1,71 @@
-<script setup>
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const todo = ref('');
+
+type Drazen = {
+  sir: string;
+  mast: number;
+};
+
 async function submit() {
-  await new Promise(r => setTimeout(r, 1000))
-  alert('Submitted! 🎉')
+  await new Promise((r) => setTimeout(r, 1000));
+  alert('Submitted! 🎉');
 }
+
+watch(todo, (newQuestion, oldQuestion) => {
+  console.log(newQuestion, oldQuestion);
+});
 </script>
 
 <template>
   <div class="your-first-form">
-    <img
-      src="https://pro.formkit.com/logo.svg"
-      alt="FormKit Logo"
-      width="244"
-      height="50"
-      class="logo"
-    >
-    <FormKit
-      type="form"
-      #default="{ value }"
-      @submit="submit"
-    >
+    <FormKit type="form">
       <FormKit
         type="text"
-        name="name"
-        label="Name"
-        help="What do people call you?"
-      />
-      <FormKit
-        type="checkbox"
-        name="flavors"
-        label="Favorite ice cream flavors"
-        :options="{
-          'vanilla': 'Vanilla',
-          'chocolate': 'Chocolate',
-          'strawberry': 'Strawberry',
-          'mint-chocolate-chip': 'Mint Chocolate Chip',
-          'rocky-road': 'Rocky Road',
-          'cookie-dough': 'Cookie Dough',
-          'pistachio': 'Pistachio',
+        label="Your todo"
+        label-class="$reset only-my-class"
+        help="Add a todo to your
+      todo list"
+        :delay="1000"
+        validation="required|length:5|email"
+        validation-visibility="live"
+        v-model="todo"
+        :help-class="{
+          'add-this-class': true,
+          'dont-add-this': false,
         }"
-        validation="required|min:2"
       />
-      
+
       <FormKit
-        type="checkbox"
-        name="agree"
-        label="I agree FormKit is the best form authoring framework."
+        type="date"
+        value="2011-01-01"
+        label="Birthday"
+        help="Enter your birth day"
+        validation="required|date_before:2010-01-01"
+        validation-visibility="live"
       />
-      <pre>{{ value }}</pre>
     </FormKit>
   </div>
 </template>
 
-<style scoped>
+<style>
+.only-my-class {
+  color: blue;
+}
+
 .your-first-form {
   width: calc(100% - 2em);
   max-width: 480px;
   box-sizing: border-box;
   padding: 2em;
-  box-shadow: 0 0 1em rgba(0, 0, 0, .1);
-  border-radius: .5em;
+  box-shadow: 0 0 1em rgba(0, 0, 0, 0.1);
+  border-radius: 0.5em;
   margin: 4em auto;
 }
 
-.logo {
-  width: 150px;
-  height: auto;
-  display: block;
-  margin: 0 auto 2em auto;
-}
 pre {
-  background-color: rgba(0, 100, 250, .1);
+  background-color: rgba(0, 100, 250, 0.1);
   padding: 1em;
 }
 </style>
